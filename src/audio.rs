05881aeebc1b88ed8eb8ddaaad648476b55cdae8
Krzysztof Langner <klangner@gmail.com>
2017-03-17 14:17:10 +0100

    play audio

--- src/audio.rs
+++ src/audio.rs
@@ -5,7 +5,7 @@
 ///  * Playing PCM data using sound card
 
 use audrey;
-use dsp::vectors::{Vector};
+use dsp::signals::{Signal};
 
 use futures::stream::Stream;
 use futures::task;
@@ -13,6 +13,7 @@ use futures::task::Executor;
 use futures::task::Run;
 
 use std;
+use std::f64::consts::PI;
 use std::sync::Arc;
 use std::thread;
 use std::time::Duration;
@@ -21,10 +22,10 @@ use cpal;
 
 
 /// Load wav file
-pub fn load(file_name: &str) -> Vector {
+pub fn load(file_name: &str) -> Signal {
     let mut reader = audrey::open(file_name).unwrap();
     let samples: Vec<f64> = reader.samples().map(Result::unwrap).collect();
-    Vector::from_reals(samples)
+    Signal::from_reals(samples, 44100)
 }
 
 /// Play PCM data using system audio
@@ -36,18 +37,27 @@ impl Executor for MyExecutor {
     }
 }
 
-pub fn play(pcm: &Vector) {
+pub fn play(pcm: &Signal) {
     let endpoint = cpal::get_default_endpoint().expect("Failed to get default endpoint");
-    let format = endpoint.get_supported_formats_list().unwrap().next().expect("Failed to get endpoint format");
+    let format = endpoint.get_supported_formats_list().unwrap()
+        .find(|x| {x.samples_rate.0 == 44100 && x.data_type == cpal::SampleFormat::F32})
+        .expect("Failed to get 44100Hz frequency");
+
+    println!("Sample rate: {:?}", format);
 
     let event_loop = cpal::EventLoop::new();
     let executor = Arc::new(MyExecutor);
 
+    // The voice can be used to control the play/pause of the output,
+    // while the samples_stream can be used to register a callback
+    // that will be called whenever the backend is ready to get data.
+    // See the documentation of futures-rs for more info about how to use streams.
     let (mut voice, stream) = cpal::Voice::new(&endpoint, &format, &event_loop).expect("Failed to create a voice");
+
 
     // Produce a sinusoid of maximum amplitude.
     let samples_rate = format.samples_rate.0 as f32;
-    let mut data_source = (0u64..).map(move |t| t as f32 * 440.0 * 2.0 * std::f64::consts::PI / samples_rate)     // 440 Hz
+    let mut data_source = (0u64..).map(move |t| t as f32 * 440.0 * 2.0 * (PI as f32) / samples_rate)     // 440 Hz
         .map(move |t| t.sin());
 
     voice.play();
@@ -78,12 +88,9 @@ pub fn play(pcm: &Vector) {
     })).execute(executor);
 
     thread::spawn(move || {
-        loop {
-            thread::sleep(Duration::from_millis(500));
-            voice.pause();
-            thread::sleep(Duration::from_millis(500));
-            voice.play();
-        }
+        voice.play();
+        thread::sleep(Duration::from_millis(3000));
+        voice.pause();
     });
 
     event_loop.run();
